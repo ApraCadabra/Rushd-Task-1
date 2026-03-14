a71a567a3b97a167f7596b779f94f5f3425fee7a
Apraham Jamil Avenian <aprahamjamil@gmail.com>
2026-03-15 00:56:41 +0300

    implemented function slugify

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
+function isAlphanumeric(character: string): boolean {
+    const code: number = character.charCodeAt(0);
+    return (
+        (code >= 48 && code <= 57) || // 0-9
+        (code >= 65 && code <= 90) || // A-Z
+        (code >= 97 && code <= 122)   // a-z
+    );
+}
+
+function isWhitespace(character: string): boolean {
+    return /\s/.test(character);
+}
+
 function slugify(input: string): string {
-    return input;
+    let slug: string = "";
+    for (const character of input) {
+        if (isAlphanumeric(character)) {
+            slug += character.toLowerCase();
+        } else if (isWhitespace(character) && slug.length > 0 && slug[slug.length - 1] !== '-') {
+            slug += "-";
+        }
+    }
+
+    if (slug.endsWith("-")) {
+        slug = slug.slice(0, -1);
+    }
+
+    return slug;
 }
 
 module.exports = slugify;
